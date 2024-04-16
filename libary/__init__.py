18f0416e0fa27e9c73b3f886742e4495183048d5
MilanLetsGetCode <167215059+MilanLetsGetCode@users.noreply.github.com>
2024-04-16 22:40:49 +0200

    Add files via upload

---
+++ libary/__init__.py
@@ -0,0 +1,41 @@
+"""
+hPyT - Hack Python Titlebar
+Author - zingzy
+version - 1.2.1
+License - MIT
+Homepage - https://github.com/zingzy/hPyT
+"""
+
+from .hPyT import (
+    all_stuffs,
+    maximize_button,
+    maximize_minimize_button,
+    minimize_button,
+    opacity,
+    rainbow_title_bar,
+    title_bar,
+    title_bar_color,
+    title_bar_text_color,
+    title_text,
+    window_animation,
+    window_flash,
+    window_frame,
+)
+
+__all__ = [
+    "title_bar",
+    "maximize_minimize_button",
+    "all_stuffs",
+    "maximize_button",
+    "minimize_button",
+    "opacity",
+    "window_flash",
+    "title_bar_color",
+    "title_bar_text_color",
+    "rainbow_title_bar",
+    "window_frame",
+    "window_animation",
+    "title_text",
+]
+
+__version__ = "1.2.1"
