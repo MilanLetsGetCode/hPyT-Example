aebe6a043272cdb299b09f887513322f021c4ed1
MilanLetsGetCode <167215059+MilanLetsGetCode@users.noreply.github.com>
2024-04-16 22:50:41 +0200

    Update example.py

---
+++ example.py
@@ -0,0 +1,27 @@
+from hPyT import *
+from customtkinter import * # pip install customtkinter
+
+'''
+----------------------------------------------
+
+           Setup, for coding
+
+----------------------------------------------
+'''
+
+window = CTk()
+
+'''
+----------------------------------------------
+
+          Put window manipulation
+
+----------------------------------------------
+'''
+
+# hPyT
+
+
+f"""{readme.md}"""
+
+window.mainloop()
